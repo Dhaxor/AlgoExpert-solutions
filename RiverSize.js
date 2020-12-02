9abd286d0de210ab9986537e08f91b5684c5fcb8
Gain John <46064597+Dhaxor@users.noreply.github.com>
2020-12-02 04:15:15 +0100

    Create RiverSize.js

---
+++ RiverSize.js
@@ -0,0 +1,59 @@
+function riverSizes(matrix) {
+	
+	let sizes = []
+	
+	let visited = matrix.map(row => row.map(value => false));
+	
+	for(let i =0; i<matrix.length; i++){
+		for(let j = 0; j< matrix[i]; j++){
+			
+			if(visited[i][j]) continue;
+			
+			  traverseNode(i,j,matrix,visited,sizes);
+			
+			
+		}
+		
+		return sizes;
+	}
+	
+}
+
+	function traverseNode(i,j,matrix,visited,sizes){
+			
+			let currentRiverSize = 0;
+			let NodestoExplore = [[i,j]];
+		
+		while(NodestoExplore.length){
+			let currentNode = NodestoExplore,pop();
+			
+			i = currentNode[0];
+			j = currentNode[1];
+			
+			if(visited[i][j]) continue;
+			visited[i][j] = true;
+			
+			if(matrix[i][j] === 0) continue;
+			currentRiverSize++;
+			const unvisitedNeighbors = getUnvisitedNeighbors(i,j,matrix,visited);
+			
+			for(let neighbor of unvisitedNeighbors){
+				NodestoExplore.push(neighbor);
+			}
+		}
+		if(currentRiverSize > 0)  sizes.push(currentRiverSize);
+		
+	}
+
+
+function getUnvisitedNeighbors(i,j,matrix,visited){
+	const unvisitedNeigbors = [];
+	if(i>0 && !visited[i-1][j]) unvisitedNeighbors.push([i-1,j]);
+	if(i < matrix.length -1 && ! visited[i + 1][k]) unvisitedNeighbors.push([i + 1,j]);
+	if(j > 0 && !visited[i][j -1]) unvisitedNeighbors.push([0, j-1]);
+	if(j < matrix[0].length -1 && !visited[i][j +1]) unvisitedNeighbors.push([i,j +1]);
+	
+	return unvisitedNeighbors
+}
+
+exports.riverSizes = riverSizes;
